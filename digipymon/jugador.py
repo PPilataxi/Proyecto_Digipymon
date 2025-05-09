71c2ebfe0211355d7b828ed40c9091cdd85b6b7c
Ppilataxi <ppt0004@alu.medac.es>
2025-05-09 16:07:22 +0200

    EmpezandoJugador
    Empezando la clase jugador para hacer y probar las cosas

--- digipymon/jugador.py
+++ digipymon/jugador.py
@@ -1,19 +1,6 @@
 class Jugador:
-    def __init__(self):
+    def __init__(self, nombre):
         self.nombre
         self.lista_digipymon = []
         self.cantidad_digypymon = 0
         self.digicoins = 10
-
-
-    
-    def añadir_digipymon(nombre):
-        
-        cantidad_digipymon += 1
-
-    def consultar_digipymon():
-        pass
-
-    def consultar_digicoins():
-        pass
-
