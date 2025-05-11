da12664d2dd521929e82d5192dfeecaea262ccd8
Ppilataxi <ppt0004@alu.medac.es>
2025-05-11 22:37:12 +0200

    Continuación sobre clase jugador

--- digipymon/jugador.py
+++ digipymon/jugador.py
@@ -1,6 +1,24 @@
 class Jugador:
+#Creación de métodos por parámetros
     def __init__(self, nombre):
-        self.nombre
+        self.nombre = nombre
         self.lista_digipymon = []
         self.cantidad_digypymon = 0
         self.digicoins = 10
+
+#Creación método para añadir digipymons
+    def añadir_digipymon(self, digipymon):
+        self.lista_digipymon.append(digipymon)
+        self.cantidad_digypymon += 1
+        print(f"{self.nombre} ha añadido a {digipymon} a su ejercito")
+
+#Creación método para consultar los digipymons que tenemos en nuestra posesión
+    def consultar_digipymon(self):
+        if self.lista_digipymon:
+            print(f"\n{self.nombre} tiene en su posesión los siguientes digipymons: ")
+            for digypymon in self.lista_digipymon:
+                print(f" {digypymon}")
+        else:
+            print(self.nombre + "no dispone de digipymons todavia")
+
+
