a193b3ff18440904b93dd7ca40955e4fed82f7ed
Ppilataxi <ppt0004@alu.medac.es>
2025-05-14 17:15:05 +0200

    Actualización clase Digipymon

--- digipymon/digipymon.py
+++ digipymon/digipymon.py
@@ -1,6 +1,5 @@
 class Digipymon():
     def __init__(self,nombre,vida,ataque,tipo,nivel):
-        pass 
         self.nombre = nombre
         self.vida = vida
         self.ataque = ataque
@@ -8,6 +7,5 @@ class Digipymon():
         self.nivel = nivel
 
 
-        def _str_():
-           
-            print(f"Nombre: {self.nombre}, Vida: {self.vida}  , Ataque:{self.ataque} , Tipo:{self.tipo} , Nivel: {self.nivel}")
+        def _str_(self):
+            return print(f"Nombre: {self.nombre}, Vida: {self.vida}  , Ataque:{self.ataque} , Tipo:{self.tipo} , Nivel: {self.nivel}")
