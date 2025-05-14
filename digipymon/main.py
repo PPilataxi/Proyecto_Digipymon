7ed735faea46245bb57893b9486c57afaaf7059c
Ppilataxi <ppt0004@alu.medac.es>
2025-05-14 16:12:20 +0200

    Base funciones del main
    Menu acabado, digishop empezado

--- digipymon/main.py
+++ digipymon/main.py
@@ -10,7 +10,17 @@ def generar_digipymon_aleatorio():
 
 
 def menu():
-    pass
+    print("*****¿Qué desea hacer?*****")
+    print("1. Buscar digipymon")
+    print("2. Luchar contra un entrenador")
+    print("3. Ir a la tienda")
+    print("4. Usar OBjetos")
+    print("5. Consultar inventario")
+    print("6. Consultar digipymon")
+    print("7. Buscar digipymon")
+
+    return menu
+    
 
 
 def buscar_digipymon():
@@ -19,8 +29,13 @@ def buscar_digipymon():
 def combate():
     pass
 
-def digishop():
-    pass
+def digishop(jugador, inventario):
+    print("*****Bienvenido a la digishop*****")
+    print("*****¿Qué desea comprar?*****")
+    print("a. Digipyballs: 5 digicoins")
+    print("b. Energetica: 3 digicoins (+ 10p digisalud)")
+    print("c. Trembolóna: 4 digicoins (+5p de aura )")
+    
 
 def usar_item():
     pass
