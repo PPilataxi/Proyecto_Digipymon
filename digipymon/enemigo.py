50b2acd0cb443b5db2a99490a064f24cbc53871a
Ppilataxi <ppt0004@alu.medac.es>
2025-05-18 18:28:51 +0200

    corrección enemigo

--- digipymon/enemigo.py
+++ digipymon/enemigo.py
@@ -6,8 +6,6 @@ class Enemigo():
         self.lista_digipymon = []
         self.cantidad_digipymon = 0
 
-    def añadir_digipymon(self,digipymon):
-
-        poyomon =  Digipymon("pollo",12,13,"fuego",2)  
-        self.lista_digipymon.append(poyomon) 
-        self.cantidad_digipymon += 1 
+    def añadir_digipymon(self, digipymon):
+        self.lista_digipymon.append(digipymon)
+        self.cantidad_digipymon += 1
